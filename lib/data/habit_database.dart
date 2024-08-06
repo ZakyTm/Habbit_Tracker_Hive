58a8d068c7c10053f5d89c7f020f2c77f95033b1
ZakyTm <zakaria.t149919@gmail.com>
2024-08-06 23:46:32 +0100

    using async and await and initHive and we opened the box of hive and creating the function that will us the CRUD Methods

--- lib/data/habit_database.dart
+++ lib/data/habit_database.dart
@@ -3,15 +3,40 @@ import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
 final _myBox = Hive.box("Habbit_Database");
+// create initial default data
+// when the user first time open the app it's called initial state
+// if it's the first time than we have to use a method or create a new method that called
+// createDefaultData() but if it's not the first time than we have to use a method
+// called LoadData() to load the data from the database
+// now if the user checked the box of the habit or edit it or delete it or add a new habit
+// than we have to use a method called updateDatabase() to update the database
+
+// tracking a habit is a daily sort of thing so we have to create a list of habits that the user
+// wants to track daily
+
+// load data if already exists
+
+// update database
 
 class HabitDatabase {
-  List todayHabitList = [
-    // create initial default data 
+  List toDayHabitList = [];
 
+  // CREATE DEFAULT DATA
+  void createDefaultData() {
+    toDayHabitList = [
+      ["Drink Water", false],
+      ["Read Book", false],
+      ["Exercise", false],
+      ["Meditate", false],
+      ["Write", false],
+    ];
+  }
 
-    // load data if already exists 
+  // LOAD DATA IF IT ALREADY EXISTS
+  void loadData() {}
 
-    // update database 
-
-  ];
+  // UPDATE DATABASE
+  void updateDatabase() {
+    _myBox.put("CURRENT_HABIT_LIST", toDayHabitList);
+  }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:habbit_tracker/components/habit_tile.dart';
 import 'package:habbit_tracker/components/my_alert_box.dart';
 import 'package:habbit_tracker/components/my_fab.dart';
+import 'package:habbit_tracker/data/habit_database.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:habbit_tracker/data/habit_database.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,44 +16,37 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // [Data structured for the habit list],
-  List toDayHabitList = [
-    ["Morning Run", false],
-    ["Reading a Book", false],
-    ["Drink Water", false],
-    ["Meditation", false],
-    ["Coding", false],
-    ["Evening Walk", false],
-  ];
+  // [Habit Name, isHabitDone]
+  HabitDatabase db = HabitDatabase();
+  final _myBox = Hive.box("Habit_Database");
+
+  @override
+  void initState() {
+    // if there is no current habit list, then it is the first time ever opening the app
+    // then create default data
+    if (_myBox.get("CURRENT_HABIT_LIST") == null) {
+      db.createDefaultData();
+
+      // there already exists data, this is not the first time
+    } else {
+      db.loadData();
+    }
+    super.initState();
+  }
 
   // CheckBox was clicked
   void checkBoxClicked(bool? value, int index) {
     setState(() {
-      toDayHabitList[index][1] = value;
+      db.toDayHabitList[index][1] = value;
     });
   }
 
   final _newHabitNameController = TextEditingController();
-  void createNewHabit() {
-    setState(() {
-      //Show alerts dialoge for use to create a fill in the new habbit
-      showDialog(
-          context: context,
-          builder: (context) {
-            return MyAlertBox(
-              controller:
-                  _newHabitNameController, //controller for the text field
-              hintText: "Enter New Habit", //hint text
-              onSave: saveNewHabit, //onSave function
-              onCancel: cancelDialogBox, //onCancel function
-            );
-          });
-    });
-  }
 
   void saveNewHabit() {
     //add a new habit to todayHabitList
     setState(() {
-      toDayHabitList.add([_newHabitNameController.text, false]);
+      db.toDayHabitList.add([_newHabitNameController.text, false]);
     });
     //clear textField
     _newHabitNameController.clear();
@@ -67,6 +64,32 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void createNewHabit() {
+    setState(() {
+      //Show alerts dialoge for use to create a fill in the new habbit
+      showDialog(
+          context: context,
+          builder: (context) {
+            return MyAlertBox(
+              controller:
+                  _newHabitNameController, //controller for the text field
+              hintText: "Enter New Habit", //hint text
+              onSave: saveNewHabit, //onSave function
+              onCancel: cancelDialogBox, //onCancel function
+            );
+          });
+    });
+  }
+
+// save existing habit with a new name
+  void saveExistingHabit(int index) {
+    setState(() {
+      db.toDayHabitList[index][0] = _newHabitNameController.text;
+      _newHabitNameController.clear();
+      Navigator.of(context).pop();
+    });
+  }
+
 //OPen habit settings to edit
   void openHabitSettings(int index) {
     showDialog(
@@ -76,24 +99,15 @@ class _HomePageState extends State<HomePage> {
             controller: _newHabitNameController,
             onSave: () => saveExistingHabit(index),
             onCancel: cancelDialogBox,
-            hintText: toDayHabitList[index][0],
+            hintText: db.toDayHabitList[index][0],
           );
         });
-  }
-
-// save existing habit with a new name
-  void saveExistingHabit(int index) {
-    setState(() {
-      toDayHabitList[index][0] = _newHabitNameController.text;
-      _newHabitNameController.clear();
-      Navigator.of(context).pop();
-    });
   }
 
 // Delete habit
   void deleteHabit(int index) {
     setState(() {
-      toDayHabitList.removeAt(index);
+      db.toDayHabitList.removeAt(index);
     });
   }
 
@@ -105,11 +119,11 @@ class _HomePageState extends State<HomePage> {
           onPressed: createNewHabit,
         ),
         body: ListView.builder(
-            itemCount: toDayHabitList.length,
+            itemCount: db.toDayHabitList.length,
             itemBuilder: (context, index) {
               return HabitTile(
-                habitName: toDayHabitList[index][0],
-                habitIsDone: toDayHabitList[index][1],
+                habitName: db.toDayHabitList[index][0],
+                habitIsDone: db.toDayHabitList[index][1],
                 toggleHabit: (value) => checkBoxClicked(value, index),
                 onEdit: (context) => openHabitSettings(index),
                 onDelete: (context) => deleteHabit(index),
